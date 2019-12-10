f8a0a2484763a8994ef6f9f951de2201df12cc2a
Marcelo Risoli <celorisoli@gmail.com>
2019-12-10 10:47:49 +0100

    rename args -> mode

--- intcode.py
+++ intcode.py
@@ -6,52 +6,52 @@ class Intcode:
     self.inputs = []
     self.o = None
 
-  def add(self, args):
-    self.a[self.value(self.i + 3, args[2], True)] = self.value(self.i + 1, args[0]) + self.value(self.i + 2, args[1])
+  def add(self, mode):
+    self.a[self.value(self.i + 3, mode[2], True)] = self.value(self.i + 1, mode[0]) + self.value(self.i + 2, mode[1])
     self.i += 4
 
-  def multiply(self, args):
-    self.a[self.value(self.i + 3, args[2], True)] = self.value(self.i + 1, args[0]) * self.value(self.i + 2, args[1])
+  def multiply(self, mode):
+    self.a[self.value(self.i + 3, mode[2], True)] = self.value(self.i + 1, mode[0]) * self.value(self.i + 2, mode[1])
     self.i += 4
 
-  def modify(self, args):
-    self.a[self.value(self.i + 1, args[0], True)] = self.inputs.pop(0)
+  def modify(self, mode):
+    self.a[self.value(self.i + 1, mode[0], True)] = self.inputs.pop(0)
     self.i += 2
 
-  def output(self, args):
-    val = self.value(self.i + 1, args[0])
+  def output(self, mode):
+    val = self.value(self.i + 1, mode[0])
     self.o = val
     self.i += 2
     print(val)
     return self.o
 
-  def jump(self, args, c):
+  def jump(self, mode, c):
     if c:
-      self.i = self.value(self.i + 2, args[1])
+      self.i = self.value(self.i + 2, mode[1])
     else:
       self.i += 3
 
-  def jump_if_true(self, args):
-    self.jump(args, self.value(self.i + 1, args[0]) != 0)
+  def jump_if_true(self, mode):
+    self.jump(mode, self.value(self.i + 1, mode[0]) != 0)
 
-  def jump_if_false(self, args):
-    self.jump(args, self.value(self.i + 1, args[0]) == 0)
+  def jump_if_false(self, mode):
+    self.jump(mode, self.value(self.i + 1, mode[0]) == 0)
 
-  def ifc(self, args, c):
+  def ifc(self, mode, c):
     if c:
-      self.a[self.a[self.i + 3]] = 1
+      self.a[self.value(self.i + 3, mode[2], True)] = 1
     else:
-      self.a[self.a[self.i + 3]] = 0
+      self.a[self.value(self.i + 3, mode[2], True)] = 0
     self.i += 4
 
-  def less_than(self, args):
-    self.ifc(args, self.value(self.i + 1, args[0]) < self.value(self.i + 2, args[1]))
+  def less_than(self, mode):
+    self.ifc(mode, self.value(self.i + 1, mode[0]) < self.value(self.i + 2, mode[1]))
 
-  def equals(self, args):
-    self.ifc(args, self.value(self.i + 1, args[0]) == self.value(self.i + 2, args[1]))
+  def equals(self, mode):
+    self.ifc(mode, self.value(self.i + 1, mode[0]) == self.value(self.i + 2, mode[1]))
 
-  def offset(self, args):
-    self.base += self.value(self.i + 1, args[0])
+  def offset(self, mode):
+    self.base += self.value(self.i + 1, mode[0])
     self.i += 2
 
   def value(self, i, n, w=False):
@@ -69,7 +69,7 @@ class Intcode:
     self.inputs += inputs
     return self
 
-  def get_args(self):
+  def mode(self):
     st = str(self.a[self.i] // 100)
     while len(st) < 3:
       st = "0" + st
@@ -78,24 +78,24 @@ class Intcode:
   def run(self):
     while True:
       op = self.get_opcode()
-      args = self.get_args()
+      mode = self.mode()
       if op == 1:
-        self.add(args)
+        self.add(mode)
       elif op == 2:
-        self.multiply(args)
+        self.multiply(mode)
       elif op == 3:
-        self.modify(args)
+        self.modify(mode)
       elif op == 4:
-        return self.output(args)
+        return self.output(mode)
       elif op == 5:
-        self.jump_if_true(args)
+        self.jump_if_true(mode)
       elif op == 6:
-        self.jump_if_false(args)
+        self.jump_if_false(mode)
       elif op == 7:
-        self.less_than(args)
+        self.less_than(mode)
       elif op == 8:
-        self.equals(args)
+        self.equals(mode)
       elif op == 9:
-        self.offset(args)
+        self.offset(mode)
       elif op == 99:
         return None
