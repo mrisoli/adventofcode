6d0d73a880f2428c32fca56a902fbdc7e879da83
Marcelo Risoli <celorisoli@gmail.com>
2019-12-12 11:10:12 +0100

    day 10 improvements

--- day10.py
+++ day10.py
@@ -1,18 +1,32 @@
 from math import atan2
 
 def sats(a, i, j):
-  sight = set()
+  sight = {}
   for ii in range(len(a)):
     for jj in range(len(a[i])):
       if a[ii][jj] == '#':
         ri = ii - i
         rj = jj - j
-        if not atan2(ri, rj) in sight:
-          sight.add(atan2(ri, rj))
-  return len(sight)
+        h = hash(atan2(ri, rj))
+        if not h in sight:
+          sight[h] = [(ii, jj)]
+        else:
+          sight[h].append((ii, jj))
+  return ((i, j), sight)
+
+def map_visible(a):
+  return [[sats(a, i, j) for j in range(len(a[i])) if a[i][j] == '#'] for i in range(len(a))]
 
 def solve(a):
-  return max([max([sats(a, i, j) for j in range(len(a[i])) if a[i][j] == '#']) for i in range(len(a))])
+  v = map_visible(a)
+  m = 0
+  for i in v:
+    for j in v:
+      k = max(j, key=lambda x: len(x[1].keys()))
+      s = len(k[1].keys())
+      if s > m:
+        m = s
+  return m
 
 a = [list(i) for i in open("in10.txt").read().splitlines()]
 print(solve(a))
