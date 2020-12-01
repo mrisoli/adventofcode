187362bc5df93998a247ef0602dee141a0f2ce7d
Marcelo Risoli <celorisoli@gmail.com>
2020-12-01 09:07:03 -0300

    small correction

--- 2020/day1/p2.py
+++ 2020/day1/p2.py
@@ -1,6 +1,6 @@
 from common import find_pair
 with open('puzzle.in') as f:
-        a = map(int, f.readlines())
+        a = list(map(int, f.readlines()))
         for i in range(len(a)):
             res = find_pair(a[i:], 2020 - a[i])
             if res:
