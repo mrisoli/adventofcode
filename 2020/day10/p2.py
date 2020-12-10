b4938b04d7f5df2869749956dac808b929b3f7a7
Marcelo Risoli <celorisoli@gmail.com>
2020-12-10 16:00:03 -0300

    minor improvements 2020 day 10 p2

--- 2020/day10/p2.py
+++ 2020/day10/p2.py
@@ -3,12 +3,9 @@ with open('puzzle.in', 'r') as f:
     list.sort(l)
     l.insert(0, 0)
     l.append(l[-1] + 3)
-    m = l[-1]
-    s = set(l)
-    a = [0] * (m + 1)
+    a = [0] * (l[-1] + 4)
     a[0] = 1
     for i in l:
         for j in range(1,4):
-            if i + j < len(a):
-                a[i + j] += a[i]
-    print(a[m])
+            a[i + j] += a[i]
+    print(a[l[-1]])
