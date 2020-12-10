5dc7a6d2bd22ae2366e6afbfa955ac4debdf742e
Marcelo Risoli <celorisoli@gmail.com>
2020-12-10 12:47:48 -0300

    improve part 2

--- 2020/day10/p2.py
+++ 2020/day10/p2.py
@@ -1,6 +1,3 @@
-from functools import lru_cache
-
-
 with open('puzzle.in', 'r') as f:
     l = list(map(int, f.readlines()))
     list.sort(l)
@@ -8,13 +5,11 @@ with open('puzzle.in', 'r') as f:
     l.append(l[-1] + 3)
     m = l[-1]
     s = set(l)
-
-@lru_cache(maxsize=len(l))
-def ways(i):
-    if i == m:
-        return 1
-    if i not in s:
-        return 0
-    return ways(i + 1) + ways(i + 2) + ways(i + 3)
-
-print(ways(0))
+    a = [0] * (m + 1)
+    a[0] = 1
+    for i in range(len(a)):
+        if i in s:
+            for j in range(1,4):
+                if i + j < len(a):
+                    a[i + j] += a[i]
+    print(a[m])
