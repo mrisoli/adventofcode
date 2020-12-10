c5484f768fc4fe5f9a66a86d50b752d0eb89e644
Marcelo Risoli <celorisoli@gmail.com>
2020-12-10 16:19:39 -0300

    minor improvements 2020 day 10 p2

--- 2020/day10/p2.py
+++ 2020/day10/p2.py
@@ -1,8 +1,8 @@
-with open('puzzle.in', 'r') as f:
-    l = [0] + list(map(int, f.readlines()))
-    list.sort(l)
-    l.append(l[-1] + 3)
-    a = [1] + ([0] * (l[-1] + 4))
-    for i in l:
-        a[i +1:i + 4] = [x + a[i] for x in a[i +1:i + 4]]
-    print(a[l[-1]])
+f = open('puzzle.in')
+l = [0] + list(map(int, f.readlines()))
+list.sort(l)
+l.append(l[-1] + 3)
+a = [1] + ([0] * (l[-1] + 4))
+for i in l:
+    a[i +1:i + 4] = [x + a[i] for x in a[i +1:i + 4]]
+print(a[l[-1]])
