6c470a0f99f1db074a4eacd370d40b5f0ac402ab
Marcelo Risoli <celorisoli@gmail.com>
2021-12-02 11:35:18 +0100

    improve 2021 python day 2 problems 1 and 2

--- python/2021/d2p2.py
+++ python/2021/d2p2.py
@@ -2,13 +2,13 @@ from utils import cmd_list
 from functools import reduce
 
 def move(p, cmd):
-    (c, d) = cmd
-    if c == 'forward':
-        return (p[0] + d, p[1] + (d * p[2]), p[2])
-    elif c == 'down':
-        return (p[0], p[1], p[2] + d)
-    else:
-        return (p[0], p[1], p[2] - d)
+    match cmd:
+        case ('forward', d):
+            return (p[0] + d, p[1] + (d * p[2]), p[2])
+        case ('down', d):
+            return (p[0], p[1], p[2] + d)
+        case ('up', d):
+            return (p[0], p[1], p[2] - d)
 
 p = reduce(move, cmd_list(2), (0,0,0))
 
