f0e492e02ad01244a5ed7e3870b52b82a3e612f3
Marcelo Risoli <celorisoli@gmail.com>
2021-12-03 10:52:32 +0100

    improve python 2021 day 3 puzzle 1 and 2

--- python/2021/d3p1.py
+++ python/2021/d3p1.py
@@ -1,10 +1,6 @@
 from utils import str_list
 from collections import Counter
 
-g,e = [],[]
-
-for n in zip(*str_list(3)):
-    gg, ee = Counter(n).most_common(2)
-    g.append(gg[0])
-    e.append(ee[0])
-print(int(''.join(g), 2) * int(''.join(e), 2))
+g = [Counter(n).most_common(1)[0][0] for n in zip(*str_list(3))]
+e = ''.join(['1' if i == '0' else '0' for i in g])
+print(int(''.join(g),2) * int(e,2))
--- python/2021/d3p2.py
+++ python/2021/d3p2.py
@@ -3,13 +3,13 @@ from collections import Counter
 
 l = str_list(3)
 
-def r(o, ty):
+def r(o, ty, i):
+    if len(o) == 1:
+        return int(o[0],2)
     s = '1' if ty == 0 else '0'
-    for i in range(len(l[0])):
-        m = Counter([r for r in zip(*o)][i]).most_common(2)
-        e = s if m[0][1] == m[1][1] else m[ty][0]
-        o = [f for f in o if f[i] == e]
-        if len(o) == 1:
-            return int(o[0],2)
+    m = Counter([r for r in zip(*o)][i]).most_common(2)
+    e = s if m[0][1] == m[1][1] else m[ty][0]
+    o = [f for f in o if f[i] == e]
+    return r(o, ty, i + 1)
 
-print(r(l[:], 0) * r(l[:], 1))
+print(r(l[:], 0, 0) * r(l[:], 1, 0))
