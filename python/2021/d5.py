919f0d1f5ad0740d12bedc6e90e73c8da3de412f
Marcelo Risoli <celorisoli@gmail.com>
2021-12-05 11:34:48 +0100

    improve day 5

--- python/2021/d5.py
+++ python/2021/d5.py
@@ -1,44 +1,21 @@
 from utils import fopen
+from collections import Counter
 
-def get_lines(f):
-    ll = []
-    for c in f.readlines():
-        ll.append(tuple(map(lambda x: tuple(map(int, x.split(','))), c.split('->'))))
-    return ll
-
-def get_diagonal(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    dx = 1 if x1 < x2 else -1
-    dy = 1 if y1 < y2 else -1
-    x,y = x1,y1
-    s = set()
-    s.add((x,y))
-    while x != x2 and y != y2:
-        x += dx
-        y += dy
-        s.add((x,y))
-    return s
-
-def get_points_between(p1, p2, diagonal):
-    x1, y1 = p1
-    x2, y2 = p2
+def points(l, d):
+    p1,p2 = l.split('->')
+    x1,y1 = map(int, p1.split(','))
+    x2,y2 = map(int, p2.split(','))
     if x1 == x2:
-        y1,y2 = min(y1,y2),max(y1,y2)
-        return set(map(lambda y: (x1, y), range(y1, y2 + 1)))
+        return [(x1, y) for y in range(min(y1,y2),max(y1,y2) + 1)]
     elif y1 == y2:
-        x1,x2 = min(x1,x2),max(x1,x2)
-        return set(map(lambda x: (x, y1), range(x1, x2 + 1)))
-    return get_diagonal(p1, p2) if diagonal else set()
+        return [(x, y1) for x in range(min(x1,x2),max(x1,x2) + 1)]
+    elif d:
+        dx = 1 if x1 < x2 else -1
+        dy = 1 if y1 < y2 else -1
+        return [(x1 + (i * dx), y1 + (i * dy)) for i in range(abs(x2 - x1) + 1)]
+    return []
 
-
-def solve(diagonal=False):
-    f = fopen(5)
-    lines = get_lines(f)
-    points = set()
-    intersection = set()
-    for (p1, p2) in lines:
-        new_points = get_points_between(p1, p2, diagonal)
-        intersection = intersection | (points & new_points)
-        points = points | new_points
-    print(len(intersection))
+def solve(d=False):
+    p = Counter()
+    [p.update(points(l, d)) for l in fopen(5).readlines()]
+    print(sum(c > 1 for c in p.values()))
