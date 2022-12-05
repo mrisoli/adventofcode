021c0023f9bacb7e5391a4fc32b61ed324ab259e
Marcelo Risoli <celorisoli@gmail.com>
2022-12-05 16:00:28 +0100

    improve python 2022 day 5

--- python/2022/d5.py
+++ python/2022/d5.py
@@ -1,16 +1,13 @@
 import re
-from itertools import groupby
 from utils import fopen
 
 def cmds_and_stacks():
-    p = [list(g) for k, g in groupby(fopen(5).readlines(), key=lambda x: x.startswith('move'))]
-    diagram = [x for x in map(str.rstrip, p[0]) if x]
-    cmds = [*map(lambda x: tuple(map(int, re.findall(r'\d+', x))), p[1])]
-    stacks = [[] for i in range(int(diagram[-1][-1]) + 1)]
-    bucket = dict([(i, int(v)) for i,v in enumerate(diagram[-1]) if v.isdigit()])
-    for v in reversed(diagram[:-1]):
-        [stacks[bucket[i]].append(c) for i,c in enumerate(v) if c.isalpha()]
-    return (cmds, stacks)
+    diagram, cmds = [s.split('\n') for s in fopen(5).read().split('\n\n')]
+    cmds = [tuple(map(int, re.findall(r'\d+', x))) for x in cmds if x]
+    st = [[] for i in range(int(diagram[-1][-1]) + 1)]
+    b = dict([(i, int(v)) for i,v in enumerate(diagram[-1]) if v.isdigit()])
+    [[st[b[i]].append(c) for i,c in enumerate(v) if c.isalpha()] for v in reversed(diagram[:-1])]
+    return (cmds, st)
 
-def answer(stacks):
-    print(''.join([s[-1] for s in stacks[1:]]))
+def answer(s):
+    print(''.join([x[-1] for x in s[1:]]))
