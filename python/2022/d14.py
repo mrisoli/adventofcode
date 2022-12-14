86891ceb85ccfd91d47da3a93140907cdf4150e6
Marcelo Risoli <celorisoli@gmail.com>
2022-12-14 15:33:30 +0100

    improve 2022 day 14 python

--- python/2022/d14.py
+++ python/2022/d14.py
@@ -3,17 +3,9 @@ from utils import str_list
 def rs(*p):
     return range(min(p), max(p) + 1)
 
-def get_floor(grid):
-    return  max([y for _,y in grid])
-
-def get_line(p1,p2):
-    return [(x,y) for x in rs(p1[0],p2[0]) for y in rs(p1[1],p2[1])]
+def get_floor(g):
+    return max([y for _,y in g])
 
 def make_grid():
-    lines = [[tuple(map(int, p.split(','))) for p in v] for v in map(lambda s: s.split('->'), str_list(14))]
-    grid = set()
-    for l in lines:
-        for p1,p2 in zip(l[:-1], l[1:]):
-            points = get_line(p1,p2)
-            grid.update(points)
-    return grid
+    lines = [zip(l[:-1], l[1:]) for l in [[tuple(map(int, p.split(','))) for p in v] for v in map(lambda s: s.split('->'), str_list(14))]]
+    return set.union(*[set.union(*[set((x,y) for x in rs(p1[0],p2[0]) for y in rs(p1[1],p2[1])) for p1,p2 in l]) for l in lines])
