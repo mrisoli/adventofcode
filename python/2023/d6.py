a97ce30ee29bec0e5cf57b76d1f70754de0ebbfd
Marcelo Risoli <celorisoli@gmail.com>
2023-12-08 11:44:10 +0530

    improve day 6

--- python/2023/d6.py
+++ python/2023/d6.py
@@ -1,8 +1,19 @@
 import re
+from math import sqrt,ceil,floor
 
 def parse_num(v):
     return re.findall('\d+', v.split(':')[1])
 
+def equation(a, b, c):
+    d = b * b - 4 * a * c
+    vsqrt = sqrt(abs(d))
+
+    l = ((-b + vsqrt)/(2 * a))
+    h = ((-b - vsqrt)/(2 * a))
+    h = int(h)
+    l = int(l) + 1 if l == int(l) else int(l)
+    return h - l
+
 def ways(w):
     t, d = w
-    return len(list(filter(lambda i: i * (t - i) > d, range(1, t))))
+    return equation(-1, t, -d)
