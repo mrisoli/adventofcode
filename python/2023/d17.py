bdbd044d735557e54b2d29f2793ba93c5036d701
Marcelo Risoli <celorisoli@gmail.com>
2023-12-27 15:00:16 +0700

    improvements day 17

--- python/2023/d17.py
+++ python/2023/d17.py
@@ -1,6 +1,15 @@
 from heapq import heappush, heappop, merge
 from utils import coords
 
+type State = tuple[int, Vertex, complex]
+
+def vector(d: complex) -> complex:
+    m = 1j if d.imag != 0 else 1
+    return -m if d.real + d.imag < 0 else m
+
+def size(v: complex) -> int:
+    return int(abs(v.real + v.imag))
+
 class Vertex:
     def __init__(self, val: complex):
         self.val = val
@@ -17,36 +26,12 @@ class Vertex:
     def __lt__(self, other):
         return False
 
-type State = tuple[int, Vertex, complex]
-type Point = tuple[complex, int]
-
-def vector(d: complex) -> complex:
-    m = 1j if d.imag != 0 else 1
-    return -m if d.real + d.imag < 0 else m
-
-def size(v: complex) -> int:
-    return int(abs(v.real + v.imag))
-
-
 class Solver:
     def __init__(self, mn: int, mx: int):
         self.g = coords(17, ''.join(map(str, range(10))), lambda x: int(x))
-        self.target = max(self.g.keys(), key=lambda x: int(x.imag)) + max(self.g.keys(), key=lambda x: int(x.real))
+        self.target = max(self.g.keys(), key=lambda x: size(x))
         self.mn = mn
         self.mx = mx
-
-
-    def neighbours(self, u: State) -> [State]:
-        c, p, v = u
-        ss = vector(v.imag + (1j * v.real))
-        ss = [-ss, ss]
-        ss.append(v + vector(v))
-        a = []
-        for vs in ss:
-            ps = p.val + vector(vs)
-            if ps in self.g and size(vs) <= self.mx:
-                a.append((c + self.g[ps], Vertex(ps), vs))
-        return a
 
     def djikstra(self) -> int:
         q: list[State] = [
@@ -54,26 +39,19 @@ class Solver:
             (self.g[1j], Vertex(1j), 1j),
         ]
         v = set()
-        s = float('inf')
         while q:
             u = heappop(q)
             cost, pos, dr = u
-            if pos.val == self.target:
+            if pos.val == self.target and size(dr) >= self.mn:
                 return cost
             if (pos.val, dr) in v:
                 continue
             v.add((pos.val, dr))
-            for n in self.neighbours(u):
-                heappush(q, n)
-
-#def jrange(p1: complex, p2: complex):
-    #d = p2 - p1
-    #m = vector(d)
-    ##d = int(abs(d.real + d.imag))
-    #for i in range(1, d + 1):
-    #    yield i * m
-
-    #def loss(self, a, b ) -> int:
-        #pa, _ = a
-        #pb, _ = b
-        #return sum(self.g[pa + v] for v in jrange(pa,pb))
+            ss = vector(dr.imag + (1j * dr.real))
+            ss = [-ss, ss]
+            ss.append(dr + vector(dr))
+            for vs in ss:
+                ps = pos.val + vector(vs)
+                sz = size(vs)
+                if ps in self.g and sz <= self.mx:
+                    heappush(q, (cost + self.g[ps], Vertex(ps), vs))
--- python/2023/d17p2.py
+++ python/2023/d17p2.py
@@ -1,15 +1,4 @@
-from d17 import run, Vertex, Graph
+from d17 import Solver
 
-def neighbours(g: Graph, u: Vertex) -> [Vertex]:
-    p, s = u
-    ss = 1 if isinstance(s, complex) else 1j
-    ss = [ss, -ss]
-    t = [(p + si, si) for si in ss if p + si in g]
-    if 0 < abs(s.real + s.imag) < 3:
-        c = s / abs(s.real + s.imag)
-        if p + c in g:
-            sp = s + c
-            t += [(p + c, s + c)]
-    return t
-
-print(run(neighbours))
+s = Solver(4, 10)
+print(s.djikstra())
