e0f9a176b9234fe9d9e6eb08a07f4bf064bfa1f5
Marcelo Risoli <celorisoli@gmail.com>
2024-12-02 07:01:25 -0500

    add python 2024 day 2 parts 1 and 2

---
+++ python/2024/d2p1.py
@@ -0,0 +1,7 @@
+from utils import rows_of_int
+
+def safe(r):
+    variation = [y - x for x,y in zip(r, r[1:])]
+    return all(x in {1,2,3} for x in variation) or all(x in {-1,-2,-3} for x in variation)
+
+print(sum(map(safe, rows_of_int(2)))
---
+++ python/2024/d2p2.py
@@ -0,0 +1,13 @@
+from utils import rows_of_int
+
+def safe(r):
+    variation = [y - x for x,y in zip(r, r[1:])]
+    if sum(x not in {1,2,3} for x in variation) <= 2 or sum(x not in {-1,-2,-3} for x in variation) <= 2:
+        for i in range(len(r)):
+            n = r[:i] + r[i+1:]
+            v = [y - x for x,y in zip(n, n[1:])]
+            if all(x in {1,2,3} for x in v) or all(x in {-1,-2,-3} for x in v):
+                return True
+    return False
+
+print(sum(map(safe, rows_of_int(2)))
